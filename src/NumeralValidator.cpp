525fec282bfd09769c9a6f7f81b8547554c9c435
RArend7 <RArend7@gmail.com>
2016-11-12 11:34:10 -0500

    Regex
    Added regular expression logic to NumeralValidator

--- src/NumeralValidator.cpp
+++ src/NumeralValidator.cpp
@@ -1,5 +1,7 @@
 #include "NumeralValidator.h"
 
+#include <regex>
+
 using namespace numcon;
 
 NumeralValidator::NumeralValidator()
@@ -14,10 +16,12 @@ NumeralValidator::~NumeralValidator()
 
 bool NumeralValidator::is_roman_numeral(std::string& input)
 {
-
+    std::regex roman_regex("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+    return std::regex_match(input, roman_regex);
 }
 
 bool NumeralValidator::is_arabic_numeral(std::string& input)
 {
-
+    std::regex arabic_regex("");
+    return std::regex_match(input, arabic_regex);
 }
