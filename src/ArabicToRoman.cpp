af0cf4b8e250687a521e5805ecd3d1ac6f8bef84
RArend7 <RArend7@gmail.com>
2016-11-12 19:39:03 -0500

    Basic Arabic to Roman
    Code will now convert Arabic numerals into Roman numerals, albeit
    without following proper rules for repetition or 'subtracting' Roman
    numerals.

--- src/ArabicToRoman.cpp
+++ src/ArabicToRoman.cpp
@@ -15,8 +15,54 @@ ArabicToRoman::~ArabicToRoman()
 
 std::string ArabicToRoman::convert(int input)
 {
-    std::stringstream ss;
-    ss << input;
-    std::string str = ss.str();
-    return str;
+    std::string output_string;
+
+    while (input > 0)
+    {
+        if (input >= 1000)
+        {
+            input -= 1000;
+            output_string += "M";
+        }
+        else if (input >= 500)
+        {
+            input -= 500;
+            output_string += "D";
+        }
+        else if (input >= 100)
+        {
+            input -= 100;
+            output_string += "C";
+        }
+        else if (input >= 50)
+        {
+            input -= 50;
+            output_string += "L";
+        }
+        else if (input >= 10)
+        {
+            input -= 10;
+            output_string += "X";
+        }
+        else if (input >= 5)
+        {
+            input -= 5;
+            output_string += "V";
+        }
+        else if (input >= 1)
+        {
+            if (input == 4)
+            {
+                input -= 4;
+                output_string += "IV";
+            }
+            else
+            {
+                input -= 1;
+                output_string += "I";
+            }
+        }
+    }
+
+    return output_string;
 }
